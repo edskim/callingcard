be7049b7a2e78c40f20220dcf40d384d2a53d75d
ed <ed@twilio.com>
2013-02-23 19:01:58 -0800

    Initial commit with controller

---
+++ app/controllers/twiml_controller.rb
@@ -0,0 +1,15 @@
+require 'twilio-ruby'
+
+class TwimlController < ApplicationController
+
+  def initiate
+  end
+
+  def pin
+
+  end
+
+  def number
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callingcard::Application.config.secret_token = '5352403e56585e35a80d2c9efe6574e0d8c6fe0db04055f8ab19f3208fc1e80987e354b821ccb3fcff8d2ed2dc26129ebc9ed01b09288b7427edb7be409d0780'
