4ceea34cbf757e4ccde03cc1431fb69e76b9ef73
ed <ed@twilio.com>
2013-02-24 14:35:55 -0800

    Committing to test on heroku

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -1,12 +1,19 @@
 require 'twilio-ruby'
 
 class TwimlController < ApplicationController
+  respond_to :xml
 
   def initiate
+    @response = Twilio::TwiML::Response.new do |r|
+      r.Gather action: "/pin", method: "GET" do |g|
+        g.Say 'Please enter your pin'
+      end
+    end
+    render xml: @response.text
   end
 
   def pin
-
+    puts params[:Digits]
   end
 
   def number
