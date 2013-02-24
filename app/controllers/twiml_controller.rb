3543c2923c8b08e9b476696a0ee536c62c70ec6c
ed <ed@twilio.com>
2013-02-24 15:08:43 -0800

    Added pin processing

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -6,14 +6,25 @@ class TwimlController < ApplicationController
   def initiate
     @response = Twilio::TwiML::Response.new do |r|
       r.Gather action: "/pin", method: "GET" do |g|
-        g.Say 'Please enter your pin'
+        g.Say 'Please enter your pin followed by a pound'
       end
     end
     render xml: @response.text
   end
 
   def pin
-    puts params[:Digits]
+    @response = Twilio::TwiML::Response.new do |r|
+      if params[:Digits] && params[:Digits].gsub(/[*#]/,'') == '24208'
+        r.Gather action: "/number", method: "GET" do |g|
+          g.Say 'Please enter the number to call followed by a pound'
+        end
+      else
+        r.Gather action: "/pin", method: "GET" do |g|
+          g.Say 'Wrong pin. Please reenter your pin followed by a pound'
+        end
+      end
+    end
+    render xml: @response.text
   end
 
   def number
